1fb64cb6d521dbdd29fcbbda74ec6ea3e1939307
Arturo Juarez <jusart.92@gmail.com>
2021-09-10 21:37:07 -0500

    SO files

---
+++ so-archivos/os.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+// console.log("CPU info: ", os.cpus());
+
+// console.log("IP addres", os.networkInterfaces());
+
+// console.log("Free memory", os.freemem());
+console.log("type", os.type());
+console.log("S0 version", os.release());
+console.log("user info", os.userInfo());
