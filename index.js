dca0a335fb8eda79faad97054eaf18a373d518e5
Arturo Juarez <jusart.92@gmail.com>
2021-09-17 21:36:50 -0500

    @hapi boom handlerError 404

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const { config } = require('./config/index');
 
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const {
+  logErrors,
+  errorHandler,
+  wrapErrors,
+} = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
 moviesApi(app);
+
+//Catch 404
+app.use(notFoundHandler);
+
+//erros midlewares
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function () {
